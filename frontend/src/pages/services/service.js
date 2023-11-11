c3d3bc6e5ff2f791c808c51876129a8deee60ebe
Odasara <e17153@eng.pdn.ac.lk>
2023-11-11 09:32:19 +0530

    Reorganize folders

--- frontend/src/pages/services/service.js
+++
@@ -1,9 +0,0 @@
-export const uploadImg =async ()=>{
-    try{
-        const response=await axios.post("http://localhost:4000/api/upload");
-        return await response.data;
-    }catch(error){
-        console.log("There is an error uploading the img");
-        return error;
-    }
-}
